9f2fe66f16012b5a3dd27ca9c4b38dc89fd0de97
Chikombe <chikombe.ecy@gmail.com>
2024-05-23 09:08:56 +0000

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,57 @@
 #!/usr/bin/python3
+"""UTF-8 validation module.
+"""
+
+
 def validUTF8(data):
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    Validate if a given dataset represents a valid UTF-8 encoding.
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
 
-    :param data: List[int] - a list of integers representing bytes
-    :return: bool - True if the data is a valid UTF-8 encoding, else False
-    """
-    n_bytes = 0
-
-    # Masks to check the leading bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for byte in data:
-        if byte > 255:
+        byte = data[i]
+        if type(byte) != int or byte < 0 or byte > 255:
             return False
 
-        mask = 1 << 7
-        if n_bytes == 0:
-            # Count the number of 1s from the most significant bit
-            while mask & byte:
-                n_bytes += 1
-             mask = mask >> 1
-            
-            if n_bytes == 0:
-                continue
-            
-            if n_bytes == 1 or n_bytes > 4:
+        # Determine the number of bytes in the character
+        if byte <= 0x7F:
+            # 1-byte character
+            skip = 0
+        elif byte & 0b11111000 == 0b11110000:
+            # 4-byte character
+            span = 4
+            if n - i >= span:
+                next_bytes = data[i + 1:i + span]
+                if not all((next_byte & 0b11000000) == 0b10000000 for next_byte in next_bytes):
+                    return False
+                skip = span - 1
+            else:
                 return False
-            
+        elif byte & 0b11110000 == 0b11100000:
+            # 3-byte character
+            span = 3
+            if n - i >= span:
+                next_bytes = data[i + 1:i + span]
+                if not all((next_byte & 0b11000000) == 0b10000000 for next_byte in next_bytes):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif byte & 0b11100000 == 0b11000000:
+            # 2-byte character
+            span = 2
+            if n - i >= span:
+                next_bytes = data[i + 1:i + span]
+                if not all((next_byte & 0b11000000) == 0b10000000 for next_byte in next_bytes):
+                    return False
+                skip = span - 1
+            else:
+                return False
         else:
-            # For remaining bytes, they must start with '10'
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-        
-        n_bytes -= 1
-
-    return n_bytes == 0
+            return False
+    return True
